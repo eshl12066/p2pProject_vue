e482aa62e4128d1c1d49b173ad73aaa8dfe60f08
javaxc <2997791530@qq.com>
2019-10-28 18:19:51 +0800

    陈钰

--- src/views/management/memberManage/MembersfManage.vue
+++ src/views/management/memberManage/MembersfManage.vue
@@ -64,9 +64,25 @@
                 fixed="right"
                 label="操作"
                 width="100">
-                <template slot-scope="scope">
-                  <el-button @click="open" type="warning" plain size="small">审核</el-button>
-                </template>
+                  <el-button type="warning" size="small" @click="dialogFormVisible = true">审核</el-button>
+
+                  <el-dialog title="收货地址" :visible.sync="dialogFormVisible">
+                    <el-form :model="form">
+                      <el-form-item label="活动名称" :label-width="formLabelWidth">
+                        <el-input v-model="form.name" autocomplete="off"></el-input>
+                      </el-form-item>
+                      <el-form-item label="活动区域" :label-width="formLabelWidth">
+                        <el-select v-model="form.region" placeholder="请选择活动区域">
+                          <el-option label="区域一" value="shanghai"></el-option>
+                          <el-option label="区域二" value="beijing"></el-option>
+                        </el-select>
+                      </el-form-item>
+                    </el-form>
+                    <div slot="footer" class="dialog-footer">
+                      <el-button @click="dialogFormVisible = false">取 消</el-button>
+                      <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+                    </div>
+                  </el-dialog>
 
               </el-table-column>
             </el-table>
@@ -152,7 +168,19 @@
             date: '2016-05-03',
             name: '王小虎',
             address: '上海市普陀区金沙江路 1518 弄'
-          }]
+          }],
+          dialogFormVisible: false,
+          form: {
+            name: '',
+            region: '',
+            date1: '',
+            date2: '',
+            delivery: false,
+            type: [],
+            resource: '',
+            desc: ''
+          },
+          formLabelWidth: '120px'
         }
 
       }
