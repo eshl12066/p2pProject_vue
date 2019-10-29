990e4b1bb44f3ca34603b3346d2da4a8d2b788e1
javazlk <452784782@qq.com>
2019-10-30 01:29:11 +0800

    曾凌康  10-30  1:28

--- src/api/action.js
+++ src/api/action.js
@@ -11,6 +11,7 @@ export default {
 
   //后台
   'SYSTEM_TREE': '/sysPermission/selectTreeNode', //树菜单
+  'SYSTEM_ASSET_RECHARGE_LISTALL': '/moneyRecharge/selectAll',  //分页模糊查找充值表
 
   //资产模块
   'SYSTEM_MENU_TREE_LIST': '/vue/treeNodeAction_selectbylist.action', //树形的另一个
